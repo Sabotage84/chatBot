77aa6a5a5528c92de91ad47301f1be1c706833be
Sabotage84 <sabotage84@mail.ru>
2020-08-05 00:21:00 +0300

    ADD onmessage bot handler

--- botWPF/botWPF/MainWindow.xaml.cs
+++ botWPF/botWPF/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -30,6 +32,30 @@ namespace botWPF
             Users = new ObservableCollection<TelegramUser>();
             userList.ItemsSource = Users;
 
+            string token = "1234567:4TT8bAc8GHUspu3ERYn-KGcvsvGB9u_n4ddy";
+            bot = new TelegramBotClient(token);
+
+            bot.OnMessage += delegate (object sender, Telegram.Bot.Args.MessageEventArgs e)
+              {
+                  string msg = $"{DateTime.Now}: {e.Message.Chat.FirstName} {e.Message.Chat.Id} {e.Message.Text}";
+
+                  File.AppendAllText("data.log", $"{msg}\n");
+
+                  Debug.WriteLine(msg);
+
+                  this.Dispatcher.Invoke(() =>
+                  {
+                      var person = new TelegramUser(e.Message.Chat.FirstName, e.Message.Chat.Id);
+                      if (!Users.Contains(person))
+                          Users.Add(person);
+                      Users[Users.IndexOf(person)].AddMessage($"{person.Nick}: {e.Message.Text}");
+                  }
+                      );
+
+              };
+            bot.StartReceiving();
+
+            //btnSendMsg.Click += delegate { SendMsg(); };
 
         }
     }
