ddb93b03ed0b9b26531faf6ef2d4a5d6a37e0d11
Sabotage84 <sabotage84@mail.ru>
2020-08-04 23:40:21 +0300

    botuser class feeled

--- botWPF/botWPF/TelegramUser.cs
+++ botWPF/botWPF/TelegramUser.cs
@@ -1,12 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace botWPF
 {
-    class TelegramUser
+    class TelegramUser : INotifyPropertyChanged, IEquatable<TelegramUser>
     {
+        public TelegramUser(string NickName, long ChatId)
+        {
+            Nick = NickName;
+            Id = ChatId;
+            Messages = new ObservableCollection<string>();
+        }
+        private string nick;
+
+        public string Nick { 
+            get => nick;
+            set {
+                this.nick = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.nick)));
+            
+            } 
+        }
+
+        public long Id { get => id;
+            set 
+            {
+                this.id = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.id)));
+
+
+            }
+        }
+
+        public ObservableCollection<string> Messages { get => messages; set => messages = value; }
+
+        private long id;
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private ObservableCollection<string> messages; 
+
+        public bool Equals(TelegramUser other)
+        {
+            return other.Id == this.Id;
+        }
+        public void AddMessage(string text)
+        {
+            Messages.Add(text);
+        }
     }
 }
